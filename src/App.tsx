9626d1477e432bd05acaaee91d89b4189aba02fb
DrVickie <116500226+drvickie@users.noreply.github.com>
2026-01-04 20:49:04 +0000

    install markdown

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import ReactMarkdown from "react-markdown";
 
 function App() {
   const [count, setCount] = useState(0)
